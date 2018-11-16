2a0e3f6d32027cda23c6d25a8daa09b92ec59652
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-11-16 23:41:44 +0100

    Handling up navigation between HomeActivity and FullScreenActivity

--- app/src/main/java/android/example/com/split/FullscreenActivity.java
+++ app/src/main/java/android/example/com/split/FullscreenActivity.java
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -23,18 +25,17 @@ import java.util.List;
  */
 public class FullscreenActivity extends AppCompatActivity {
 
+    public static final int DELAY_MILLIS = 2000;
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
      */
     private static final boolean AUTO_HIDE = true;
-
     /**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
      */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
@@ -91,10 +92,90 @@ public class FullscreenActivity extends AppCompatActivity {
     };
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.activity_fullscreen);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        constraintLayout.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        final Button signInButton = (Button) findViewById(R.id.button_sign_in);
+        signInButton.setVisibility(View.GONE);
+
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (auth.getCurrentUser() != null) {
+                    Intent intent = new Intent(FullscreenActivity.this, HomeActivity.class);
+                    intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    signInButton.setVisibility(View.VISIBLE);
+                    signInButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Not signed in
+                            // Firebase UI Authentication
+                            // Choose authentication providers
+                            List<AuthUI.IdpConfig> providers = Collections
+                                    .singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+                            // Create and launch sign-in intent
+                            startActivityForResult(
+                                    AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers)
+                                          .build(), RC_SIGN_IN);
+                        }
+                    });
+                }
+            }
+        };
+        handler.removeCallbacks(runnable);
+        handler.postDelayed(runnable, DELAY_MILLIS);
+
 
         mVisible = true;
         //        mControlsView = findViewById(R.id.fullscreen_content_controls);
@@ -112,22 +193,6 @@ public class FullscreenActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.button_sign_in).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // Not signed in
-                // Firebase UI Authentication
-                // Choose authentication providers
-                List<AuthUI.IdpConfig> providers = Collections
-                        .singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
-
-                // Create and launch sign-in intent
-                startActivityForResult(
-                        AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
-                        RC_SIGN_IN);
-                return false;
-            }
-        });
     }
 
     @Override
@@ -141,8 +206,7 @@ public class FullscreenActivity extends AppCompatActivity {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Toast.makeText(this, "Signed in as " + user.getDisplayName(), Toast.LENGTH_LONG).show();
-
-                Intent intent = new Intent(this, HomeActivity.class);
+                Intent intent = new Intent(FullscreenActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
                 // ...
