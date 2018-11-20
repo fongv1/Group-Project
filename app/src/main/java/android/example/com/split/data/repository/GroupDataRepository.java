417d96699b4ad1cac7f837e120b232015c54c76a
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-11-20 14:24:33 +0100

    Deleted commented code in GroupDataRepository

--- app/src/main/java/android/example/com/split/data/repository/GroupDataRepository.java
+++ app/src/main/java/android/example/com/split/data/repository/GroupDataRepository.java
@@ -5,16 +5,17 @@ import android.util.Log;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
 public class GroupDataRepository {
 
     private static final String TAG = "DataRepository";
+
     public void getUser() {
 
     }
 
 
-
-    public void createNewGroup(Group group){
+    public void createNewGroup(Group group) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("groups").add(group).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
@@ -23,40 +24,4 @@ public class GroupDataRepository {
             }
         });
     }
-
-
-
-
-
-    //               FirebaseAuth auth = FirebaseAuth.getInstance();
-    //        auth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-    //            @Override
-    //            public void onComplete(@NonNull Task<AuthResult> task) {
-    //                if (task.isSuccessful()) {
-    //
-    //                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    //
-    //                    User myUser = new User();
-    //                    myUser.setId("12345678");
-    //                    myUser.setEmail("saleh@ibra.com");
-    //                    myUser.setFirstName("Saleh");
-    //                    myUser.setLastName("Ibrahim");
-    //                    myUser.setPhoneNumber("0732222222");
-    //
-    //                    DataRepository dataRepository = new DataRepository();
-    //                    dataRepository.createNewUser(myUser);
-    //                    Toast.makeText(HomeActivity.this,"HELL",Toast.LENGTH_LONG).show();
-    //                } else {
-    //                    Toast.makeText(HomeActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
-    //                }
-    //            }
-    //        });
-
-    //        FirebaseFirestore db = FirebaseFirestore.getInstance();
-    //        db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
-    //            @Override
-    //            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-    //
-    //            }
-    //        });
 }
