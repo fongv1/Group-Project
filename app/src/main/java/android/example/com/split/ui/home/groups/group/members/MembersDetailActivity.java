81b0c82ac79d52277e8f97ccaf241d3c6667db29
Vin <fongv1@hotmail.com>
2018-11-21 17:57:00 +0100

    passing member data working

--- app/src/main/java/android/example/com/split/ui/home/groups/group/members/MembersDetailActivity.java
+++ app/src/main/java/android/example/com/split/ui/home/groups/group/members/MembersDetailActivity.java
@@ -1,30 +1,16 @@
 package android.example.com.split.ui.home.groups.group.members;
 
 import android.example.com.split.R;
-import android.example.com.split.ui.home.groups.group.expenses.ExpensesTabFragment;
+import android.example.com.split.data.entity.User;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MembersDetailActivity extends AppCompatActivity {
 
+    private User user;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -36,6 +22,13 @@ public class MembersDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_members);
 
+        Bundle bundle = getIntent().getExtras();
 
+        if (bundle != null){
+           user =  (User) bundle.get("user");
+        }
+
+        TextView textView = findViewById(R.id.member_detail_name);
+        textView.setText(user.getFirstName());
     }
 }
