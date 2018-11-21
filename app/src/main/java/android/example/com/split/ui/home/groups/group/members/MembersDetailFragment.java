f141ce9051e9f5b38de16468c29296f8af961bbd
Vin <fongv1@hotmail.com>
2018-11-21 11:20:50 +0100

    member_detail_fragments_created

---
+++ app/src/main/java/android/example/com/split/ui/home/groups/group/members/MembersDetailFragment.java
@@ -0,0 +1,43 @@
+package android.example.com.split.ui.home.groups.group.members;
+
+import android.example.com.split.R;
+import android.example.com.split.data.entity.User;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MembersDetailFragment extends Fragment {
+
+    private static final String TAG = "MembersTabFragment";
+    private List<User> dataset;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        initDataset();
+
+        View rootView = inflater.inflate(R.layout.fragment_tab_members, container, false);
+
+        return rootView;
+    }
+
+    // Create dummy data
+    private void initDataset() {
+        dataset = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            User user = new User();
+            user.setFirstName("Member " + i);
+            dataset.add(user);
+        }
+    }
+}
