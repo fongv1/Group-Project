c4d447bdf8c964af5eadcd061e9b71bd605bb249
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-11-22 13:42:26 +0100

    Test for Split singleton creation

--- lib/src/test/java/com/alaskalany/lib/SplitTest.java
+++ lib/src/test/java/com/alaskalany/lib/SplitTest.java
@@ -1,5 +1,7 @@
 package com.alaskalany.lib;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 class SplitTest {
 
     private Split split;
@@ -15,5 +17,8 @@ class SplitTest {
 
     @org.junit.jupiter.api.Test
     void getInstance() {
+        Split expected = split;
+        Split actual = Split.getInstance();
+        assertSame(expected, actual);
     }
 }
