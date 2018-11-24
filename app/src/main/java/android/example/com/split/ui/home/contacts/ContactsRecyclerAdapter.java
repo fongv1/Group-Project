6278c3c5e3b3788071ac537b7ed5998e5c482b8c
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-11-24 15:16:32 +0100

    Refactored ContactViewHolder to do bindings in the holder not the recycler adapter

--- app/src/main/java/android/example/com/split/ui/home/contacts/ContactsRecyclerAdapter.java
+++ app/src/main/java/android/example/com/split/ui/home/contacts/ContactsRecyclerAdapter.java
@@ -12,78 +12,73 @@ import android.widget.TextView;
 import java.util.List;
 
 
-// Simple implementation for a data set that consists of a List of Strings displayed using TextView widgets
-public class ContactsRecyclerAdapter extends RecyclerView.Adapter<ContactsRecyclerAdapter.ContactViewHolder> {
+// Simple implementation for a data set that consists of a List of Strings displayed using
+// TextView widgets
+public class ContactsRecyclerAdapter extends RecyclerView.Adapter<ContactsRecyclerAdapter
+    .ContactViewHolder> {
 
-    private List<User> mDataset;
+  private List<User> mDataset;
 
-    // Create the adapter with a dataset
-    public ContactsRecyclerAdapter(List<User> myDataset) {
-        mDataset = myDataset;
+  // Create the adapter with a dataset
+  public ContactsRecyclerAdapter(List<User> myDataset) {
+    mDataset = myDataset;
+  }
+
+  public List<User> getDataset() {
+    return mDataset;
+  }
+
+  // Create new views (invoked by the layout manager)
+  @Override
+  public ContactViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+    View v = (View) LayoutInflater.from(parent.getContext())
+                                  .inflate(R.layout.item_contact, parent, false);
+    ContactViewHolder vh = new ContactViewHolder(v, this);
+    return vh;
+  }
+
+  // Replace the contents of a view (invoked by the layout manager)
+  @Override
+  public void onBindViewHolder(ContactViewHolder holder, int position) {
+    // - get element from your dataset at this position
+    // - replace the contents of the view with that element
+    holder.bind(mDataset.get(position));
+  }
+
+  // Return the size of your dataset (invoked by the layout manager)
+  @Override
+  public int getItemCount() {
+    return mDataset.size();
+  }
+
+  // Provides reference to the views for each data item
+  // When create more complex group view, it should be removed in a separate java file
+  class ContactViewHolder extends RecyclerView.ViewHolder {
+
+    // Each group data item is just a String presented as a textView in this case
+    private final TextView mTextView;
+    private final View itemView;
+
+    // Initializes the ViewHolder TextView from the item_group XML resource
+    public ContactViewHolder(View itemView, ContactsRecyclerAdapter adapter) {
+      super(itemView);
+      this.itemView = itemView;
+      mTextView = (TextView) itemView.findViewById(R.id.textView_contact_item);
     }
 
-    public List<User> getDataset() {
-        return mDataset;
-    }
-
-    // Create new views (invoked by the layout manager)
-    @Override
-    public ContactViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View v = (View) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
-        ContactViewHolder vh = new ContactViewHolder(v, this);
-        return vh;
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(ContactViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        User user = mDataset.get(position);
-        holder.mTextView.setText(user.getFirstName());
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return mDataset.size();
-    }
-
-    // Provides reference to the views for each data item
-    // When create more complex group view, it should be removed in a separate java file
-    class ContactViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-        final ContactsRecyclerAdapter mAdapter;
-        // Each group data item is just a String presented as a textView in this case
-        public TextView mTextView;
-
-        // Initializes the ViewHolder TextView from the item_group XML resource
-        public ContactViewHolder(View v, ContactsRecyclerAdapter adapter) {
-            super(v);
-            mTextView = (TextView) v.findViewById(R.id.textView_contact_item);
-            this.mAdapter = adapter;
-            v.setOnClickListener(this);
-        }
-
+    public void bind(final User user) {
+      mTextView.setText(user.getFirstName());
+      itemView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            // Get the position of the item that was clicked.
-            int mPosition = getLayoutPosition();
-            // Use that to access the affected item in mDataset.
-            User user = mDataset.get(mPosition);
-            // Show toast when clicked
-            //Toast.makeText(v.getContext(), "Clicked " + user.getFirstName(), Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(v.getContext(), ContactDetailActivity.class);
-            intent.putExtra("user", user);
-
-            // Notify the adapter, that the data has changed so it can
-            // update the RecyclerView to display the data.
-            mAdapter.notifyDataSetChanged();
-            v.getContext().startActivity(intent);
-
+          Intent intent = new Intent(v.getContext(), ContactDetailActivity.class);
+          intent.putExtra("user", user);
+          v.getContext().startActivity(intent);
         }
+      });
     }
+  }
 }
 
 
