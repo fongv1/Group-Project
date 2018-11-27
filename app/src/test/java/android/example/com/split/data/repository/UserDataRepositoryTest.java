0f58174f86144241f5cfb72b33f7d0fa5f6907ce
SalehIbr <saleh.info85@gmail.com>
2018-11-27 11:36:00 +0100

    Created test classes for data repositories

---
+++ app/src/test/java/android/example/com/split/data/repository/UserDataRepositoryTest.java
@@ -0,0 +1,44 @@
+package android.example.com.split.data.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserDataRepositoryTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void createNewUser() {
+  }
+
+  @Test
+  public void addNewContact() {
+  }
+
+  @Test
+  public void getDocumentId() {
+  }
+
+  @Test
+  public void isUserExist() {
+  }
+
+  @Test
+  public void addNewContact1() {
+  }
+
+  @Test
+  public void getContactlist() {
+  }
+
+  @Test
+  public void getUserDetail() {
+  }
+}
