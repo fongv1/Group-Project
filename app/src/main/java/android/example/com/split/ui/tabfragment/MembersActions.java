be79a2339f857bc70efb6a67212d61b02357cdf0
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-11-28 14:42:48 +0100

    Added methods to group members actions interface

--- app/src/main/java/android/example/com/split/ui/tabfragment/MembersActions.java
+++ app/src/main/java/android/example/com/split/ui/tabfragment/MembersActions.java
@@ -1,5 +1,35 @@
 package android.example.com.split.ui.tabfragment;
 
+import android.example.com.split.data.entity.Group;
+import android.example.com.split.data.entity.User;
+
+import java.util.List;
+
 public interface MembersActions {
 
+  void addNewMember(User member);
+
+  List<User> getContactsNotInAGroup(Group group, List<User> contacts);
+
+  User selectContactFromContacts(List<User> contacts, User contact);
+
+  boolean isContactMemberInGroup(User contact, Group group);
+
+  User initializeNewGroupMember(User newMember, Group group);
+
+  void saveNewMemberInGroup(User newMember, Group group);
+
+  void saveNewMemberInGroupToRemoteDb(User newMember, Group group);
+
+  void updateUiAfterAddingNewMemberToGroup();
+
+  void removeGroupMember(User member, Group group);
+
+  boolean groupMemberHasZeroBalanceInGroup(User user, Group group);
+
+  void removeGroupMemberInRemoteDb(User member, Group group);
+
+  void updateUiAfterRemovingGroupMember();
+
+  
 }
