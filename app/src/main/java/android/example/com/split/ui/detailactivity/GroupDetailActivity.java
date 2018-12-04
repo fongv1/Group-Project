cc81a10612b78869937096cdacf4b9010a4fc0d8
Fatemeh <fatemeh.engqvist@gmail.com>
2018-12-04 17:05:23 +0100

    settle up

--- app/src/main/java/android/example/com/split/ui/detailactivity/GroupDetailActivity.java
+++ app/src/main/java/android/example/com/split/ui/detailactivity/GroupDetailActivity.java
@@ -32,6 +32,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 
 import java.io.Serializable;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.*;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -103,7 +105,7 @@ public class GroupDetailActivity extends BaseDetailActivity {
       @Override
       public void onClick(View v) {
 
-        showPopup();
+        showPopup(v);
       }
     });
 
@@ -490,6 +492,7 @@ public class GroupDetailActivity extends BaseDetailActivity {
   public Double getShare() {
     if (group.getMembers().size() != 0) {
       share = sum / group.getUserMembers().size();
+
     }
     return share;
   }
@@ -503,22 +506,25 @@ public class GroupDetailActivity extends BaseDetailActivity {
 
   public String getResultString(){
     String result = "";
+
+    //String.format("%.0f", doubleValue)
+
     if(!getShare().isNaN()) {
-      result = "The share for each person is: " + getShare() + "\n";
+
+      result = result + "The share for each person is " + String.format("%.0f",getShare()) + " SEK \n";
+
       Iterator it = baseHashMap.entrySet().iterator();
       while (it.hasNext()) {
         Map.Entry pair = (Map.Entry)it.next();
+
         Double rest = (Double) pair.getValue() - getShare();
+
         if(rest > 0) {
-          result = result + pair.getKey() + " should get paid " + rest + "\n";
-        }
-
-        else if (rest < 0) {
-          result = result + pair.getKey() + " should pay " + rest * -1 + "\n";
-        }
-
-        else {
-          result = result + pair.getKey() + " should pay nothing \n";
+          result = result + pair.getKey() + " should get paid " + String.format("%.0f",rest) + " SEK \n";
+        } else if (rest < 0) {
+          result = result + pair.getKey() + " should pay " + String.format("%.0f",rest * -1) + " SEK \n";
+        } else {
+          result = result + pair.getKey() + " should pay nothing \n \n";
         }
 
         it.remove(); // avoids a ConcurrentModificationException
@@ -530,9 +536,14 @@ public class GroupDetailActivity extends BaseDetailActivity {
   }
 
 
-  private void showPopup() {
+  private void showPopup(View v) {
     //if you call this method correctly then you do not need to wrap
     // this method by try-catch block which affects performance
+
+    dialogBuilder = new AlertDialog.Builder(this);
+
+    dialogBuilder.setView(v);
+    dialog = dialogBuilder.create();
 
     LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
@@ -556,5 +567,6 @@ public class GroupDetailActivity extends BaseDetailActivity {
     //show popup window after you have done initialization of views
     pwindo.showAtLocation(layout, Gravity.CENTER, 0, 0);
   }
+
 
 }
